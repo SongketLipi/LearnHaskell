08351d67b9a8b50782f490bb09f15919d76509ed
showrav-ansary <showrav.ansary.bd@gmail.com>
2021-08-07 01:35:42 +0600

    Basics

--- List.hs
+++ List.hs
@@ -2,15 +2,20 @@
 listA = [69,420,121]
 listB = [121,1,2]
 
+
 -- Concat multiple list by adding `++` between them
 listC = listA ++ listB
 
 stringA = "alpha"
 stringB = "beta"
+stringAB = stringA ++ stringB -- concating strings
+
 
 charListA = ['a','b','c','d']
 charListB = ['c','d','e','f']
 
+stringFromList = charListA ++ charListB -- concating list of characters returns a string
+
 -- prepend an element by adding it to list by putting `:` before the list
-charListC = 'a':charListA
-prepending = 1:2:3:[]
+charListPrepending = 'a':charListA
+prependingToEmptyList = 1:2:3:[]
